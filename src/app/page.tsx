f1e387a137d2b4670a84090b7503a6a65006df74
Sean O'Hara <sohara@sohara.com>
2023-08-11 14:23:43 -0400

    Add theme with dark/light theme switching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "../components/mode-toggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+        Hello World
+      </h1>
       <Button>Click Me</Button>
+      <ModeToggle />
     </main>
-  )
+  );
 }
