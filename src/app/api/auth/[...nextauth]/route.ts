cce9dde101f5743eb969a07c2e53539f86acb897
Sean O'Hara <sohara@sohara.com>
2023-08-13 14:24:31 -0400

    Use @next-auth/prisma-adapter instead of @auth/prisma-adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { AuthOptions, DefaultSession } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({ log: ["query"] });
